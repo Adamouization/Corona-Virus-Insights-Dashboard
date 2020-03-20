3990f0a2637eb5679a4b89d119322bdbb9005673
Matthew Drago <matthewdrago@pop-os.localdomain>
2020-03-20 15:44:54 +0000

    get countries

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 // const csv = Papa.parse(url)
 // console.log(csv)
-d3.csv('./data/covid_timeline.csv').then((data) => {
-  window.cases = data
-})
+d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+  .then((data) => {
+    window.cases = data
+  })
+
+window.getByCountry = country => window.cases.filter(item => item['Country/Region'].toLowerCase() === country.toLowerCase())
