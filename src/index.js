92f32c2cf45de6491e908b4bb182ee3549078180
Matthew Drago <matthewdrago@pop-os.localdomain>
2020-03-20 15:05:51 +0000

    Data handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello World");
+// import url from './data/covid_timeline.csv'
+
+// const csv = Papa.parse(url)
+// console.log(csv)
+d3.csv('./data/covid_timeline.csv').then((data) => {
+  window.cases = data
+})
