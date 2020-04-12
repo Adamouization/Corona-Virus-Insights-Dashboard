df0089c9d8c7b070c1bb6d9e9936f03fa953ba10
Adam Jaamour <adam@jamour.com>
2020-04-12 19:37:43 +0200

    X-axis added to line graph (thank you malak)

--- src/line-graph.js
+++ src/line-graph.js
@@ -1,7 +1,37 @@
 const getCasesOnDay = (cases, dateStr) => cases.map(country => Number(country[dateStr])).reduce((prev, next) => prev + next)
 
-const populateLineGraph = async (cases) => {
+const populateLineGraph = async (lineGraphDom, cases, dates) => {
+  let dateExtent = d3.extent(dates.reverse())
 
+  let newDates = d3.timeDay.range(new Date(dateExtent[0]), new Date(dateExtent[1]));
+  console.log(newDates)
+
+  let xScale = d3.scaleBand()
+    .domain(newDates.map(function (d) {
+      return d
+    }))
+    .range([0, 500])
+  let xAxis = d3.axisBottom(xScale)
+  console.log(xScale)
+
+  let lineGraphInstance = d3.select(lineGraphDom)
+    .append("svg")
+    .attr("width", "100vh")
+    .attr("height", "100vh");
+
+  lineGraphInstance.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(" + 0 + "," + 520 + ")")
+    .call(xAxis.tickFormat(d3.timeFormat("%m/%d/%y"))
+      .tickValues(xScale.domain()
+        .filter(function( d, i ) {
+            return !(i % 9)
+          })))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-0.8em")
+    .attr("dy", "0.15em")
+    .attr("transform", "rotate(-90)")
 }
 
 export {
