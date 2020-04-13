308100e2fbced5a4b56b2335195a838cf1e49871
Adam Jaamour <adam@jamour.com>
2020-04-13 13:47:42 +0200

    Add line to the svg element

--- src/line-graph.js
+++ src/line-graph.js
@@ -3,9 +3,8 @@ import {getCasesOnDay} from "./utils.js"
 const populateLineGraph = async (lineGraphDom, cases, dates) => {
   let margin = 50
 
-  // Reverse dates to have them in chronological order, and get the first and last date.
+  // Reverse dates to have them in chronological order.
   dates.reverse()
-  let dateExtent = d3.extent(dates)
 
   // Calculate the number of new confirmed cases on a daily basis.
   let totalCasesPerDay = []
@@ -19,7 +18,7 @@ const populateLineGraph = async (lineGraphDom, cases, dates) => {
 
   // Prepare the X axis for the dates.
   // Build a new array of dates (Date objects instead of strings).
-  let datesArr = d3.timeDay.range(new Date(dateExtent[0]), new Date(dateExtent[1]));
+  let datesArr = d3.timeDay.range(new Date(dates[0]), new Date(dates[dates.length - 1]));
   let xScale = d3.scaleBand()
     .domain(datesArr.map(function (d) {
       return d
@@ -27,11 +26,21 @@ const populateLineGraph = async (lineGraphDom, cases, dates) => {
     .range([0, 500 - margin])
   let xAxis = d3.axisBottom(xScale)
 
+  // Prepare the Y axis for the number of daily cases.
   let dailyCasesExtent = d3.extent(dailyEvolution)
   let yScale = d3.scaleLinear()
     .domain(dailyCasesExtent)
     .range([520, 0])
   let yAxis = d3.axisLeft(yScale)
+
+  // Prepare the line graph data.
+  const line = d3.line()
+    .x(function (d, i) {
+      return xScale(datesArr[i])
+    })
+    .y(function (d, i) {
+      return yScale(dailyEvolution[i])
+    })
 
   // Instantiate the SVG for the line graph.
   let lineGraphInstance = d3.select(lineGraphDom)
@@ -39,25 +48,34 @@ const populateLineGraph = async (lineGraphDom, cases, dates) => {
     .attr("width", "100vh")
     .attr("height", "100vh");
 
-  // Add the X axis to the svg. Only add a label every 9 days. Rotate labels to fit axis.
+  // Add the X axis to the svg. Only add a label every 8 days. Rotate labels to fit axis.
   lineGraphInstance.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(" + margin + "," + 520 + ")")
-    .call(xAxis.tickFormat(d3.timeFormat("%m/%d/%y"))
+    .call(xAxis
+      .tickFormat(d3.timeFormat("%m/%d/%y"))
       .tickValues(xScale.domain()
         .filter(function (d, i) {
-          return !(i % 9)
+          return !(i % 8)
         })))
     .selectAll("text")
     .style("text-anchor", "end")
     .attr("dx", "-0.8em")
-    .attr("dy", "0.15em")
-    .attr("transform", "rotate(-90)")
+    .attr("dy", "-0.15em")
+    .attr("transform", "rotate(-65)")
 
+  // Add the Y axis to the svg.
   lineGraphInstance.append("g")
     .attr("class", "y axis")
     .attr("transform", "translate(" + margin + ",0)")
     .call(yAxis)
+
+  // Add the line to the svg.
+  lineGraphInstance.append("path")
+    .datum(datesArr)
+    .attr("class", "line")
+    .attr("d", line)
+    .attr("transform", "translate(" + margin + ",0)")  // translate by the same amount as the Y axis.
 }
 
 export {
